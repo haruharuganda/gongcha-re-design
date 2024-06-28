261d4f4420bbf064191bf2c4cfa31225c71da25a
haruharuganda <97508841+haruharuganda@users.noreply.github.com>
2024-06-28 13:20:36 +0900

    router 연결중

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,7 +1,15 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/About'
 
-function App() {
-  return <></>
+const App = () => {
+  return (
+    <Routes>
+      <Route
+        path="*"
+        element={<Home />}
+      />
+    </Routes>
+  )
 }
 
 export default App
--- code/src/main.jsx
+++ code/src/main.jsx
@@ -1,9 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import root from './routes/root'
+import { RouterProvider } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const rootNode = document.getElementById('root')
+ReactDOM.createRoot(rootNode).render(<RouterProvider router={root} />)
