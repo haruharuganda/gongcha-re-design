d639d3a5336cc9a69a6e45218ff201da945c545d
haruharuganda <97508841+haruharuganda@users.noreply.github.com>
2024-07-02 12:05:47 +0900

    scroll event

--- code/src/components/organism/Header.jsx
+++ code/src/components/organism/Header.jsx
@@ -1,10 +1,29 @@
 import { Link } from 'react-router-dom';
 import Navigation from '../molecule/Navigation';
 import logo from '../../assets/img/logo.png';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
+    <header className={isScrolled ? 'scrolled' : undefined}>
       <h1>
         <span>공차</span>
         <Link to="/">
