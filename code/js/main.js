ad2b778b61fcc32a077793f26c0554f0834c54cd
haruharuganda <97508841+haruharuganda@users.noreply.github.com>
2024-07-11 12:24:21 +0900

    menu
    반응형완료

--- code/js/main.js
+++ code/js/main.js
@@ -1,26 +1,4 @@
-const main = new Swiper('.main-carousel .swiper', {
-   speed: 400,
-   spaceBetween: 100,
-   loop: true,
 
-   autoplay: true,
-});
-
-const menu = new Swiper('.menu-description .mySwiper', {
-   slidesPerView: 3,
-   centeredSlides: true,
-   spaceBetween: 30,
-   rewind: true,
-   autoplay: true,
-   pagination: {
-      el: '.swiper-pagination',
-      type: 'progressbar',
-   },
-   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-   },
-});
 
 $(function () {
    $('.sub-menu').hide();
