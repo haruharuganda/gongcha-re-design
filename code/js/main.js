3fa7c64435b2afbae6a535807082f4cadd337b3d
haruharuganda <97508841+haruharuganda@users.noreply.github.com>
2024-07-22 09:34:08 +0900

    order
    1. order tablet version container 중앙배열 방식 margin->display:flex로 변경

--- code/js/main.js
+++ code/js/main.js
@@ -27,4 +27,26 @@ $(function () {
       }
       debounceTimeout = setTimeout(handleScroll, 100);
    });
+
+   $(document).ready(function () {
+      // 스크롤 이벤트 설정
+      $(window).scroll(function () {
+         // 특정 뷰포트 위치 계산 (예: 화면의 중간)
+         var viewportTop = $(window).scrollTop();
+         var viewportBottom = viewportTop + $(window).height();
+         var triggerAt = viewportBottom + 150; // 오브젝트가 보여질 위치 (여기서는 화면 아래에서 200px 위)
+
+         // 오브젝트의 위치 계산
+         $('.scroll-spy').each(function () {
+            var objectTop = $(this).offset().top;
+
+            // 오브젝트가 특정 뷰포트에 들어왔을 때 애니메이션 적용
+            if (objectTop < triggerAt) {
+               $(this).addClass('animate'); // animate 클래스를 추가하여 애니메이션 효과를 줌
+            } else {
+               $(this).removeClass('animate');
+            }
+         });
+      });
+   });
 });
